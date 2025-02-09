5da748d55edda5f9329adfe76f145238c685838c
Anoop Sharma <anoopksharma22@gmail.com>
2025-02-10 01:07:46 +0530

    Editor converted to a component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,87 +1,16 @@
-import { useState } from "react";
-import Editor from "@monaco-editor/react";
 import "./App.css";
+
+import CodeEditor from "./components/CodeEditor.jsx";
 import Navbar from "./components/Navbar.jsx";
-import {RiResetLeftLine} from "react-icons/ri";
 
 const App = () => {
-    const [code, setCode] = useState(`// Do not rename to class name
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-    }
-}`);
-    const [output, setOutput] = useState("Waiting for output...");
-    const [loading, setLoading] = useState(false);
-    const handleReset = async () => {
-        setCode(`// Do not rename to class name
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, Java!");
-    }
-}`);
-        setOutput("Waiting for output...");
-
-    };
-    const handleRunCode = async () => {
-        setLoading(true);
-        setOutput("Running...");
-
-        try {
-            const response = await fetch("http://localhost:8080/api/execute", {
-                method: "POST",
-                headers: { "Content-Type": "text/plain" },
-                body: code
-            });
-
-            const result = await response.json();
-            if( result.output !== "" ) {
-                setOutput(result.output || "No output returned");
-            }else{
-                setOutput(result.error || "No output returned");
-            }
-
-        } catch (error) {
-            setOutput(`Error: ${error.message}`);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    return (
+    return(
         <div className="main-container">
             <Navbar />
-            <div className="editor-container">
-                <h3 className="title">Java code editor</h3>
-                <div className="editor">
-                    <Editor
-                        height="100%"
-                        defaultLanguage="java"
-                        value={code}
-                        theme="vs-dark"
-                        options={{
-                            fontSize: "16px", // Set font size dynamically
-                        }}
-                        onChange={(value) => setCode(value)}
-                    />
-                    <div className="editor-buttons">
-                        <RiResetLeftLine onClick={handleReset} className="reset-button" />
-                        <button className="run-button" onClick={handleRunCode} disabled={loading}>
-                            {loading ? "Running..." : "Run Code"}
-                        </button>
-                    </div>
-                </div>
-
-                <div className="output">
-                    <h3>Output:</h3>
-                    <pre className="output-text">
-                        {output}
-                    </pre>
-                </div>
-            </div>
+            <CodeEditor/>
         </div>
-
     );
+
 };
 
 export default App;
---
+++ frontend/src/components/CodeEditor.jsx
@@ -0,0 +1,83 @@
+import {useState} from "react";
+import Editor from "@monaco-editor/react";
+import {RiResetLeftLine} from "react-icons/ri";
+import "./code_editor.css";
+const CodeEditor = () => {
+    const [code, setCode] = useState(`// Do not rename to class name
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}`);
+    const [output, setOutput] = useState("Waiting for output...");
+    const [loading, setLoading] = useState(false);
+    const handleReset = async () => {
+        setCode(`// Do not rename to class name
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Java!");
+    }
+}`);
+        setOutput("Waiting for output...");
+
+    };
+    const handleRunCode = async () => {
+        setLoading(true);
+        setOutput("Running...");
+
+        try {
+            const response = await fetch("http://localhost:8080/api/execute", {
+                method: "POST",
+                headers: { "Content-Type": "text/plain" },
+                body: code
+            });
+
+            const result = await response.json();
+            if( result.output !== "" ) {
+                setOutput(result.output || "No output returned");
+            }else{
+                setOutput(result.error || "No output returned");
+            }
+
+        } catch (error) {
+            setOutput(`Error: ${error.message}`);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    return (
+        <>
+            <div className="editor-container">
+                <h3 className="title">Java code editor</h3>
+                <div className="editor">
+                    <Editor
+                        height="100%"
+                        defaultLanguage="java"
+                        value={code}
+                        theme="vs-dark"
+                        options={{
+                            fontSize: "16px", // Set font size dynamically
+                        }}
+                        onChange={(value) => setCode(value)}
+                    />
+                    <div className="editor-buttons">
+                        <RiResetLeftLine onClick={handleReset} className="reset-button" />
+                        <button className="run-button" onClick={handleRunCode} disabled={loading}>
+                            {loading ? "Running..." : "Run Code"}
+                        </button>
+                    </div>
+                </div>
+
+                <div className="output">
+                    <h3>Output:</h3>
+                    <pre className="output-text">
+                        {output}
+                    </pre>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default CodeEditor;
